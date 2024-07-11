4363fe5c799a8dc381b204f3e5c97f8fedcac600
Ed Knutson <knutsoned+github@gmail.com>
2024-07-11 09:13:53 -0500

    add menu items

--- src/setup/menu.rs
+++ src/setup/menu.rs
@@ -32,6 +32,15 @@ pub fn build_menu(
                 menu.separator();
 
                 menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Help"),
+                    shortcut: vec![KeyCode::KeyH].into(),
+                    alt_code: KeyCode::KeyH.into(),
+                    ..default()
+                }).insert(Page::CameraControl);
+
+                menu.separator();
+
+                menu.menu_item(MenuItemConfig {
                     name: l10n.lbl("CameraControl"),
                     shortcut: vec![KeyCode::KeyC].into(),
                     alt_code: KeyCode::KeyC.into(),
@@ -66,7 +75,7 @@ pub fn build_menu(
         bar.menu(
             MenuConfig {
                 name: l10n.lbl("File"),
-                alt_code: KeyCode::KeyL.into(),
+                alt_code: KeyCode::KeyF.into(),
             },
             |menu| {
                 menu.menu_item(MenuItemConfig {
@@ -93,6 +102,148 @@ pub fn build_menu(
         );
 
         bar.separator();
+
+        bar.menu(
+            MenuConfig {
+                name: l10n.lbl("Edit"),
+                alt_code: KeyCode::KeyE.into(),
+            },
+            |menu| {
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Copy"),
+                    shortcut: vec![KeyCode::KeyC].into(),
+                    alt_code: KeyCode::KeyC.into(),
+                    ..default()
+                });
+
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Cut"),
+                    shortcut: vec![KeyCode::KeyX].into(),
+                    alt_code: KeyCode::KeyX.into(),
+                    ..default()
+                });
+
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Paste"),
+                    shortcut: vec![KeyCode::KeyV].into(),
+                    alt_code: KeyCode::KeyV.into(),
+                    ..default()
+                });
+            }
+        );
+
+        bar.separator();
+
+        bar.menu(
+            MenuConfig {
+                name: l10n.lbl("Selection"),
+                alt_code: KeyCode::KeyS.into(),
+            },
+            |menu| {
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("SelectAll"),
+                    shortcut: vec![KeyCode::KeyA].into(),
+                    alt_code: KeyCode::KeyA.into(),
+                    ..default()
+                });
+
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("SelectNone"),
+                    shortcut: vec![KeyCode::KeyD].into(),
+                    alt_code: KeyCode::KeyD.into(),
+                    ..default()
+                });
+            }
+        );
+
+        bar.separator();
+
+        bar.menu(
+            MenuConfig {
+                name: l10n.lbl("View"),
+                alt_code: KeyCode::KeyV.into(),
+            },
+            |_menu| {}
+        );
+
+        bar.separator();
+
+        bar.menu(
+            MenuConfig {
+                name: l10n.lbl("Activity"),
+                alt_code: KeyCode::KeyA.into(),
+            },
+            |menu| {
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Start"),
+                    shortcut: vec![KeyCode::KeyO].into(),
+                    alt_code: KeyCode::KeyO.into(),
+                    ..default()
+                });
+
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Restart"),
+                    shortcut: vec![KeyCode::KeyS].into(),
+                    alt_code: KeyCode::KeyS.into(),
+                    ..default()
+                });
+
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Stop"),
+                    shortcut: vec![KeyCode::KeyC].into(),
+                    alt_code: KeyCode::KeyC.into(),
+                    ..default()
+                });
+            }
+        );
+
+        bar.separator();
+
+        bar.menu(
+            MenuConfig {
+                name: l10n.lbl("Run"),
+                alt_code: KeyCode::KeyR.into(),
+            },
+            |menu| {
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Debug"),
+                    shortcut: vec![KeyCode::KeyD].into(),
+                    alt_code: KeyCode::KeyD.into(),
+                    ..default()
+                });
+
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Stop debugging"),
+                    shortcut: vec![KeyCode::KeyS].into(),
+                    alt_code: KeyCode::KeyS.into(),
+                    ..default()
+                });
+
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Build release"),
+                    shortcut: vec![KeyCode::KeyR].into(),
+                    alt_code: KeyCode::KeyR.into(),
+                    ..default()
+                });
+
+                menu.menu_item(MenuItemConfig {
+                    name: l10n.lbl("Profile"),
+                    shortcut: vec![KeyCode::KeyP].into(),
+                    alt_code: KeyCode::KeyP.into(),
+                    ..default()
+                });
+            }
+        );
+
+        bar.separator();
+
+        bar.menu(
+            MenuConfig {
+                name: l10n.lbl("Window"),
+                alt_code: KeyCode::KeyW.into(),
+            },
+            |_menu| {}
+        );
 
         bar.extra_menu(|extra| {
             extra
